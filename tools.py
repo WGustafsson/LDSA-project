f0d8f33d1c5873ac0531633ffb88c0b89432cb10
Kasper Ramström <kasper.ram@gmail.com>
2018-05-22 18:37:44 +0200

    moved spark config + parquet file reading to separate python file

---
+++ tools.py
@@ -0,0 +1,23 @@
+import os
+from pyspark.sql import SparkSession
+
+def setup_spark_config(appName="Exploring Million Song Dataset"):
+    spark = SparkSession \
+    .builder \
+    .appName(appName) \
+    .getOrCreate()
+
+    sc = spark.sparkContext
+    return sc, spark
+
+def read_parquet_files(basedir, spark):
+    if basedir is None:
+        print("No provided directory")
+        return
+    elif os.path.isdir(basedir):
+        print("Reading songs from parquet files to DataFrame")
+        songs_df = spark.read.parquet(basedir)
+        return songs_df
+    else:
+        print("No parquet files to read in provided directory, aborting")
+        return
